7a8dfcef9d64f455ea6c34cc31dd9257797101f7
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2023-02-05 21:58:52 -0500

    4.1 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Float variable for speed
+    public float speed = 5;
+
+    //Variable to call the Rigidbody
+    private Rigidbody playerRb;
+
+    //Variable to call the focalPoint that the main camera is attached to
+    private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody gets initialized
+        playerRb = GetComponent<Rigidbody>();
+        //Getting the Values and finding the Focal Point GameObject
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Float Variable to get the values of the (W/Up & S/Down) movement 
+        float forwardInput = Input.GetAxis("Vertical");
+        //Adds force to the Rigidbody of the player and makes it move with the focal point 
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
